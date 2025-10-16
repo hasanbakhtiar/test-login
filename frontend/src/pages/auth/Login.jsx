a0f09d6f883c96731064887cf805bebd8578dd0f
Hasan Bakhtiar <hasan.doctype@gmail.com>
2025-10-16 16:07:57 +0400

    version 2

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,20 +1,38 @@
 
+import axios from 'axios';
+import { useRef } from 'react';
+import { Link } from 'react-router-dom';
+
 const Login = () => {
+
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const loginForm = e => {
+    e.preventDefault();
+    axios.post('http://localhost:3000/login', {
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+    }).then(res => console.log(res)
+    ).catch(err => console.log(err))
+  }
+
   return (
     <>
       <div className="d-flex align-items-center justify-conent-center flex-column">
         <h2 className="my-5 alert alert-success col-3 text-center">Login</h2>
         <div className="col-3">
-          <form>
+          <form onSubmit={loginForm}>
             <div className="mb-3">
               <label className="form-label">Email address</label>
-              <input type="email" className="form-control" />
+              <input type="email" className="form-control" ref={emailRef} />
             </div>
             <div className="mb-3">
               <label className="form-label">Password</label>
-              <input type="password" className="form-control" />
+              <input type="password" className="form-control" ref={passwordRef} />
             </div>
-            <button type="submit" className="btn btn-warning">Login</button>
+            <button type="submit" className="btn btn-warning">Login</button> <br />
+            <Link to="/register"> Register now</Link>
           </form>
         </div>
       </div>
--- frontend/src/pages/auth/Register.jsx
+++ frontend/src/pages/auth/Register.jsx
@@ -1,28 +1,48 @@
 
+import axios from 'axios';
+import { useRef } from 'react';
+import { Link } from 'react-router-dom';
+
 const Register = () => {
+  const fullnameRef = useRef(null);
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const registerForm = e => {
+    e.preventDefault();
+    axios.post('http://localhost:3000/register', {
+      fullname: fullnameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+    }).then(res => console.log(res)
+    ).catch(err => console.log(err))
+  }
+
   return (
     <>
       <div className="d-flex align-items-center justify-conent-center flex-column">
         <h2 className="my-5 alert alert-success col-3 text-center">Register</h2>
         <div className="col-3">
-          <form>
+          <form onSubmit={registerForm}>
 
             <div className="mb-3">
               <label className="form-label">Fullname</label>
-              <input type="text" className="form-control" />
+              <input type="text" className="form-control" ref={fullnameRef} />
             </div>
 
             <div className="mb-3">
               <label className="form-label">Email address</label>
-              <input type="email" className="form-control" />
+              <input type="email" className="form-control" ref={emailRef} />
             </div>
 
             <div className="mb-3">
               <label className="form-label">Password</label>
-              <input type="password" className="form-control" />
+              <input type="password" className="form-control" ref={passwordRef} />
             </div>
 
             <button type="submit" className="btn btn-warning">Register</button>
+            <br />
+            <Link to="/"> Login now</Link>
           </form>
         </div>
       </div>
